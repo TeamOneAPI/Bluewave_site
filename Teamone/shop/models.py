080f8994635d025c3f384297963201dde9a65ae3
NwachiDaniel <sechendudaniel@gmail.com>
2025-09-07 22:46:44 +0100

    editing creating models

--- Teamone/shop/models.py
+++ Teamone/shop/models.py
@@ -10,3 +10,16 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email or self.username
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to="products/", null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    stock = models.IntegerField(default=10)
+
+    def __str__(self):
+        return self.name
