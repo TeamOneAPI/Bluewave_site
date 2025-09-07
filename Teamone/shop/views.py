4ffdc3b0494cd05fe767b362dcbb00b44308536b
NwachiDaniel <sechendudaniel@gmail.com>
2025-09-07 22:54:35 +0100

    creating views

--- Teamone/shop/views.py
+++ Teamone/shop/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
-# Create your views here.
+import stripe
+import json
+from decimal import Decimal, InvalidOperation
+from django.shortcuts import render, get_object_or_404, HttpResponse, redirect
+from django.core.paginator import Paginator
+from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.utils import timezone
+from datetime import timedelta
+from django.contrib.auth import get_user_model
+from django.views.decorators.http import require_POST
