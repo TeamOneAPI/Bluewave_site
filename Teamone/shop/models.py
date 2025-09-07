3b7a818a7d8bfb60f21bc7bba8d4385cf7254980
NwachiDaniel <sechendudaniel@gmail.com>
2025-09-07 22:45:43 +0100

    creating models

--- Teamone/shop/models.py
+++ Teamone/shop/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+from decimal import Decimal
+import uuid
 
-# Create your models here.
+
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return self.email or self.username
